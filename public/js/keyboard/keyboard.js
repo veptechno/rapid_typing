ce2aa29ac4c441e5f778c6c5ab20562dc2d7b251
Protasov Vitalii <veptechno@gmail.com>
2022-08-19 14:53:10 +0300

    WIP

--- public/js/keyboard/keyboard.js
+++ public/js/keyboard/keyboard.js
@@ -9,17 +9,8 @@ export default class Keyboard {
     updateColors() {
         for (let letter in this.lettersElements) {
             let score = this.lettersService.getScore(letter)
-            // let x = score * 10
-            // let hue = -0.2443 * x * x * x + 4.37 * x * x + 4.6215 * x - 3.886
-            // if (hue < 0) {
-            //     hue = 0
-            // }
-            // if (hue > 240) {
-            //     hue = 240
-            // }
             let hue = score * 240
             this.lettersElements[letter].style.backgroundColor = "hsl(" + hue + ", 50%, 50%)"
-            this.lettersElements[letter].innerText = letter + " " + score.toFixed(1)
         }
     }
 
